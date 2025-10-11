a2021f174adbe531aee830854e154ce4499ce791
saramohamed512 <saramohamed12656@gmail.com>
2025-10-11 14:33:53 +0300

    Repository Design Pattern

---
+++ GymSystemDAL/Repositroies/Classes/MemberRepository.cs
@@ -0,0 +1,46 @@
+﻿using GymSystemDAL.Data.Context;
+using GymSystemDAL.Entities;
+using GymSystemDAL.Repositroies.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystemDAL.Repositroies.Classes
+{
+    internal class MemberRepository : IMemberRepository
+    {
+        //Connect to DB
+        private readonly GymSystemDBContext _dbContext = new GymSystemDBContext();
+        public int Add(Member member)
+        {
+            _dbContext.Members.Add(member);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(int id)
+        {
+            var member = _dbContext.Members.Find(id);
+            if (member is null) return 0;
+         
+            _dbContext.Members.Remove(member);
+            return _dbContext.SaveChanges();
+
+        }
+
+        public IEnumerable<Member> GetAll()  => _dbContext.Members.ToList();
+        
+
+        public Member? GetById(int id) => _dbContext.Members.Find(id);
+
+        
+       
+
+        public int Update(Member member)
+        {
+            _dbContext.Members.Update(member);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ GymSystemDAL/Repositroies/Interfaces/IMemberRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystemDAL.Repositroies.Interfaces
+{
+    internal interface IMemberRepository
+    {
+
+        IEnumerable<Entities.Member> GetAll();
+        Entities.Member? GetById(int id);
+
+        int Add(Entities.Member member);
+
+        int Delete(int id);
+
+        int Update(Entities.Member member);
+
+    }
+}
