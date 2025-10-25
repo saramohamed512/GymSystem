83c1e4c9e939a6922fe7af6d813375017f06acc8
saramohamed512 <saramohamed12656@gmail.com>
2025-10-25 15:26:44 +0300

    Plan Controller [Edit]

--- GymSystemPL/Controllers/PlanController.cs
+++ GymSystemPL/Controllers/PlanController.cs
@@ -1,6 +1,8 @@
 ﻿using GymSystemBLL.Services.Classes;
 using GymSystemBLL.Services.Interfaces;
+using GymSystemBLL.ViewModels.PlanViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace GymSystemPL.Controllers
 {
@@ -34,6 +36,41 @@ namespace GymSystemPL.Controllers
                  return RedirectToAction("Index");
            }
            return View(Plan);
+
+        }
+        #endregion
+        #region Edit Plan
+        public ActionResult Edit(int id)
+        {
+            if (id <= 0)
+            {
+                TempData["ErrorMessage"] = "Id Cannot Be 0 or Negative Number !";
+                return RedirectToAction("Index");
+            }
+            var Plan = _planService.GetPlanToUpdate(id);
+            if (Plan == null)
+            {
+                TempData["ErrorMessage"] = "Plan Not Found !";
+                return RedirectToAction("Index");
+            }
+            return View(Plan);
+        }
+        [HttpPost]
+        public ActionResult Edit([FromRoute] int id, UpdatePlanViewModel UpdatedPlan )
+        {
+            if (!ModelState.IsValid)
+            {
+               ModelState.AddModelError( "Wrong Data !" ,"Check Data Again !");
+                return View(UpdatedPlan);
+            }
+            var isUpdated = _planService.UpdatePlan(id, UpdatedPlan);
+            if (!isUpdated)
+            {
+                TempData["ErrorMessage"] = "Failed to Update Plan !";
+                return RedirectToAction("Index");
+            }
+            TempData["SuccessMessage"] = "Plan Updated Successfully !";
+            return RedirectToAction("Index");
 
         }
         #endregion
