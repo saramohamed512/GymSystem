7da6ae5074e9e8d39af0f0eb2f6b007a1b1becdc
saramohamed512 <saramohamed12656@gmail.com>
2025-10-25 16:40:06 +0300

    Session Controller [Details]

--- GymSystemPL/Controllers/SessionController.cs
+++ GymSystemPL/Controllers/SessionController.cs
@@ -19,6 +19,23 @@ namespace GymSystemPL.Controllers
             return View(Sessions);
         }
         #endregion
+        #region Get Session Details
+        public ActionResult Details(int id)
+        {
+            if(id <= 0)
+            {
+                TempData["ErrorMessage"] = "Invalid Session Id.";
+                return RedirectToAction("Index");
+            }
+            var session = _sessionService.GetSessionById(id);
+            if (session == null)
+            {
+               TempData["ErrorMessage"] = "Session not found.";
+                return RedirectToAction("Index");
+            }
+            return View(session);
+        }
+        #endregion
 
     }
 }
